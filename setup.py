6163c5d97b9df732937b81fde50fcd6604880a19
Andrew Collier <andrew@exegetic.biz>
2020-08-28 19:32:59 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 PACKAGE_NAME = 'trundlerpy'
 AUTHOR = 'Laura Calcagni, Andrew B. Collier'
 AUTHOR_EMAIL = 'l.calcagni@gmail.com, andrew@exegetic.biz'
