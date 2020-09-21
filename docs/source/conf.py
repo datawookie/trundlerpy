e41c8725bdea8cf854d59fd56034ba5a5c57cd11
Laura Calcagni <l.calcagni@gmail.com>
2020-09-21 15:00:00 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Andrew Collier, Laura Calcagni'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['m2r']
+extensions = ['m2r2']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
