dc4bdce23a10598979537f5ad80a0fe881a86a1c
Laura Calcagni <l.calcagni@gmail.com>
2020-09-21 14:38:44 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,7 @@ author = 'Andrew Collier, Laura Calcagni'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,12 +48,9 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 master_doc = 'index'
 
-extensions = [
-    'm2r2'
-]
 # source_suffix = '.rst'
 source_suffix = ['.rst', '.md']
